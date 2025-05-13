420991ae06f2dbc84de3e95c0a2b778c852cc357
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-05-13 02:03:04 -0500

    Creación del UserService

---
+++ usuario-backend/usuario-backend/src/main/java/com/kaimaki/usuario/usuariobackend/service/UserService.java
@@ -0,0 +1,31 @@
+package com.kaimaki.usuario.usuariobackend.service;
+
+import com.kaimaki.usuario.usuariobackend.dto.UserRegistroDTO;
+import com.kaimaki.usuario.usuariobackend.model.User;
+import com.kaimaki.usuario.usuariobackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User registrarUsuario(UserRegistroDTO dto) throws Exception {
+        if (userRepository.findByCorreo(dto.getCorreo()).isPresent()) {
+            throw new Exception("El correo ya está registrado.");
+        }
+
+        User user = new User();
+        user.setNombre(dto.getNombre());
+        user.setApellido(dto.getApellido());
+        user.setCorreo(dto.getCorreo());
+        user.setContrasena(dto.getContrasena()); // Puedes encriptar luego
+        user.setTelefono(dto.getTelefono());
+        user.setRolId(dto.getRolId());
+        user.setEstadoId(dto.getEstadoId());
+
+        return userRepository.save(user);
+    }
+}
