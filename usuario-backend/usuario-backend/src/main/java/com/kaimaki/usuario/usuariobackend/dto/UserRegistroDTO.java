fa68ab817826b4aa31f72ca57068ba298c849e28
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-05-13 01:56:58 -0500

    Creación del DTO de registro

---
+++ usuario-backend/usuario-backend/src/main/java/com/kaimaki/usuario/usuariobackend/dto/UserRegistroDTO.java
@@ -0,0 +1,70 @@
+package com.kaimaki.usuario.usuariobackend.dto;
+
+public class UserRegistroDTO {
+
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String contrasena;
+    private String telefono;
+    private Integer rolId;
+    private Integer estadoId;
+
+    // Getters y Setters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Integer getRolId() {
+        return rolId;
+    }
+
+    public void setRolId(Integer rolId) {
+        this.rolId = rolId;
+    }
+
+    public Integer getEstadoId() {
+        return estadoId;
+    }
+
+    public void setEstadoId(Integer estadoId) {
+        this.estadoId = estadoId;
+    }
+}
