63851515116c46a8618974d4bf042beab35c158c
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-05-19 22:50:53 -0500

    modifica TrabajadorService para mostrar los datos de trabajadores

--- usuario/usuario-backend/src/main/java/com/kaimaki/usuario/usuariobackend/controller/TrabajadorController.java
+++ usuario/usuario-backend/src/main/java/com/kaimaki/usuario/usuariobackend/controller/TrabajadorController.java
@@ -1,4 +1,21 @@
 package com.kaimaki.usuario.usuariobackend.controller;
 
+import com.kaimaki.usuario.usuariobackend.dto.TrabajadorDTO;
+import com.kaimaki.usuario.usuariobackend.service.TrabajadorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
 public class TrabajadorController {
+
+    @Autowired
+    private TrabajadorService trabajadorService;
+
+    @GetMapping("/disponibles")
+    public ResponseEntity<List<TrabajadorDTO>> listarTrabajadoresDisponibles(){
+        List<TrabajadorDTO> lista = trabajadorService.obtenerTrabajadoresDisponibles();
+        return ResponseEntity.ok(lista);
+    }
 }
