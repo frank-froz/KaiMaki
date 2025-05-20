a7e826e7aa1b7c58b1bce0b2ebbde7564cfba521
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-05-19 22:40:04 -0500

    configura TrabajadorRepository con la query para mostrar datos del trabajador

--- usuario/usuario-backend/src/main/java/com/kaimaki/usuario/usuariobackend/repository/TrabajadorRepository.java
+++ usuario/usuario-backend/src/main/java/com/kaimaki/usuario/usuariobackend/repository/TrabajadorRepository.java
@@ -1,4 +1,13 @@
 package com.kaimaki.usuario.usuariobackend.repository;
 
-public class TrabajadorRepository {
+import com.kaimaki.usuario.usuariobackend.model.Trabajador;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface TrabajadorRepository extends JpaRepository<Trabajador, Long> {
+
+    @Query
+    List<Trabajador> findTrabajadoresVerificados();
 }
