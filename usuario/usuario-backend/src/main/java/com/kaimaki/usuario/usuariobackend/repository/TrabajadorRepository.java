7c42cc77ff7b2577b942c4d5e8bb2e99f0cc1ca9
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-05-31 19:32:28 -0500

    Agrega el repository de trabajador

--- usuario/usuario-backend/src/main/java/com/kaimaki/usuario/usuariobackend/repository/TrabajadorRepository.java
+++ usuario/usuario-backend/src/main/java/com/kaimaki/usuario/usuariobackend/repository/TrabajadorRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 public interface TrabajadorRepository extends JpaRepository<Trabajador, Long> {
 
-    @Query
+    @Query("SELECT t FROM Trabajador t WHERE t.estadoVerificacion.nombre = 'Verificado'")
     List<Trabajador> findTrabajadoresVerificados();
 }
