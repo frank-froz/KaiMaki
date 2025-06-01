f7b922a18a3dd9d74b750827c9b1a9cc40342093
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-05-31 19:29:12 -0500

    Agrega el endpoint de vista y  busqueda de trabajadores

--- usuario/usuario-backend/src/main/java/com/kaimaki/usuario/usuariobackend/controller/TrabajadorController.java
+++ usuario/usuario-backend/src/main/java/com/kaimaki/usuario/usuariobackend/controller/TrabajadorController.java
@@ -5,9 +5,14 @@ import com.kaimaki.usuario.usuariobackend.service.TrabajadorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/api/trabajadores")
 public class TrabajadorController {
 
     @Autowired
@@ -17,5 +22,11 @@ public class TrabajadorController {
     public ResponseEntity<List<TrabajadorDTO>> listarTrabajadoresDisponibles(){
         List<TrabajadorDTO> lista = trabajadorService.obtenerTrabajadoresDisponibles();
         return ResponseEntity.ok(lista);
+    }
+
+    @GetMapping("/buscar")
+    public ResponseEntity<List<TrabajadorDTO>> buscarPorOficio(@RequestParam String oficio) {
+        List<TrabajadorDTO> resultado = trabajadorService.obtenerTrabajadoresPorOficio(oficio);
+        return ResponseEntity.ok(resultado);
     }
 }
