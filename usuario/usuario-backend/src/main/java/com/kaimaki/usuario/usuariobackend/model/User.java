9ea5eddb878cd9c2c66658d8076678dcf9d87286
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-05-31 19:32:02 -0500

    Agrega la conexión de la tabla ubicación con user

--- usuario/usuario-backend/src/main/java/com/kaimaki/usuario/usuariobackend/model/User.java
+++ usuario/usuario-backend/src/main/java/com/kaimaki/usuario/usuariobackend/model/User.java
@@ -6,6 +6,13 @@ import jakarta.persistence.*;
 @Table(name = "users")
 public class User {
 
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Ubicacion ubicacion;
+
+    public Ubicacion getUbicacion() { return ubicacion; }
+
+    public void setUbicacion(Ubicacion ubicacion) { this.ubicacion = ubicacion; }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
