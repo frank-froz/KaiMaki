a7bb9cfc6235e2bf76f6f8e6756624ef8ec33689
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-05-31 19:31:08 -0500

    Agrega el modelo trabajador con sus gettes y setters

--- usuario/usuario-backend/src/main/java/com/kaimaki/usuario/usuariobackend/model/Trabajador.java
+++ usuario/usuario-backend/src/main/java/com/kaimaki/usuario/usuariobackend/model/Trabajador.java
@@ -3,6 +3,8 @@ package com.kaimaki.usuario.usuariobackend.model;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.List;
+
 
 @Entity
 @Table(name = "trabajadores")
@@ -26,4 +28,60 @@ public class Trabajador {
 
     @OneToMany(mappedBy = "trabajador")
     private List<TrabajadorOficio> oficios;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getAntecedentes() {
+        return antecedentes;
+    }
+
+    public void setAntecedentes(String antecedentes) {
+        this.antecedentes = antecedentes;
+    }
+
+    public EstadoVerificacion getEstadoVerificacion() {
+        return estadoVerificacion;
+    }
+
+    public void setEstadoVerificacion(EstadoVerificacion estadoVerificacion) {
+        this.estadoVerificacion = estadoVerificacion;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public List<TrabajadorOficio> getOficios() {
+        return oficios;
+    }
+
+    public void setOficios(List<TrabajadorOficio> oficios) {
+        this.oficios = oficios;
+    }
 }
