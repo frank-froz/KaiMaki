a8ac4d483cafb5b14170514b1c2d75f852255a57
Han-28 <hector.castro@tecsup.edu.pe>
2025-06-18 13:26:35 -0500

    feat: permitir obtener perfil por ID para usuarios autenticados

--- usuario/usuario-backend/src/main/java/com/kaimaki/usuario/usuariobackend/controller/PerfilController.java
+++ usuario/usuario-backend/src/main/java/com/kaimaki/usuario/usuariobackend/controller/PerfilController.java
@@ -4,6 +4,8 @@ import com.kaimaki.usuario.usuariobackend.dto.PerfilDTO;
 import com.kaimaki.usuario.usuariobackend.service.PerfilService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,9 +15,13 @@ public class PerfilController {
     @Autowired
     private PerfilService perfilService;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<PerfilDTO> obtenerPerfil(@PathVariable Long id) {
-        PerfilDTO dto = perfilService.obtenerPerfil(id);
+    @PreAuthorize("hasAnyRole('CLIENTE')")
+    @GetMapping
+    public ResponseEntity<PerfilDTO> obtenerPerfilActual(Authentication authentication) {
+        String correo = authentication.getName(); // obtenido del token
+        PerfilDTO dto = perfilService.obtenerPerfilPorCorreo(correo);
         return ResponseEntity.ok(dto);
     }
+
+
 }
