fce891b42b19ac5339786617edd46aa640934e5d
Han-28 <hector.castro@tecsup.edu.pe>
2025-06-17 23:09:48 -0500

    Refactor: Actualización de TrabajadorController para usar nueva estructura de servicios

--- usuario/usuario-backend/src/main/java/com/kaimaki/usuario/usuariobackend/controller/TrabajadorController.java
+++ usuario/usuario-backend/src/main/java/com/kaimaki/usuario/usuariobackend/controller/TrabajadorController.java
@@ -2,12 +2,12 @@ package com.kaimaki.usuario.usuariobackend.controller;
 
 import com.kaimaki.usuario.usuariobackend.dto.TrabajadorDTO;
 import com.kaimaki.usuario.usuariobackend.service.TrabajadorService;
+import com.kaimaki.usuario.usuariobackend.service.impl.TrabajadorServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,12 +18,29 @@ public class TrabajadorController {
     @Autowired
     private TrabajadorService trabajadorService;
 
+    @PreAuthorize("hasRole('CLIENTE')")
     @GetMapping("/disponibles")
     public ResponseEntity<List<TrabajadorDTO>> listarTrabajadoresDisponibles(){
+        //líneas para depuración
+        System.out.println(" Solicitud autenticada por: " + SecurityContextHolder.getContext().getAuthentication().getName());
+        System.out.println(" Roles: " + SecurityContextHolder.getContext().getAuthentication().getAuthorities());
+
         List<TrabajadorDTO> lista = trabajadorService.obtenerTrabajadoresDisponibles();
         return ResponseEntity.ok(lista);
     }
+    @PreAuthorize("hasRole('CLIENTE')")
+    @GetMapping("/{id}")
+    public ResponseEntity<TrabajadorDTO> obtenerPorId(@PathVariable Long id) {
+        TrabajadorDTO dto = trabajadorService.obtenerTrabajadorPorId(id);
+        if (dto == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(dto);
+    }
 
+
+
+    @PreAuthorize("hasRole('CLIENTE')")
     @GetMapping("/buscar")
     public ResponseEntity<List<TrabajadorDTO>> buscarPorOficio(@RequestParam String oficio) {
         List<TrabajadorDTO> resultado = trabajadorService.obtenerTrabajadoresPorOficio(oficio);
