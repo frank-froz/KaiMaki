8c103131116fc919e3fb09a19ac66337bd4a60f7
Han-28 <hector.castro@tecsup.edu.pe>
2025-06-17 23:11:37 -0500

    Feat: Inclusión del rol de usuario en el contexto de autenticación del frontend

--- usuario/usuario-frontend-web/src/context/AuthContext.jsx
+++ usuario/usuario-frontend-web/src/context/AuthContext.jsx
@@ -2,23 +2,35 @@ import React, { createContext, useState, useEffect } from 'react'
 import { jwtDecode } from 'jwt-decode';
 
 
-
 export const AuthContext = createContext(null)
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const token = localStorage.getItem('token')
+    console.log('[AuthContext] Token encontrado en localStorage:', token)
+
     if (token) {
       try {
         const decoded = jwtDecode(token)
+        console.log('[AuthContext] Usuario decodificado:', decoded)
+
+        if (decoded.exp * 1000 < Date.now()) {
+          console.warn('⚠️ Token expirado')
+          throw new Error('Token expirado')
+        }
+
         setUser(decoded)
+        console.log('[AuthContext] Usuario seteado en contexto')
       } catch (error) {
-        console.error('Token inválido', error)
+        console.error('[AuthContext] Token inválido al cargar:', error)
         logout()
       }
     }
+
+    setLoading(false)
   }, [])
 
   function login(token) {
@@ -33,7 +45,7 @@ export function AuthProvider({ children }) {
   }
 
   return (
-      <AuthContext.Provider value={{ user, login, logout }}>
+      <AuthContext.Provider value={{ user, loading, login, logout }}>
         {children}
       </AuthContext.Provider>
   )
