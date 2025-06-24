a4a2962c88e82f4289c25cb71d62541910f44912
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-06-24 00:06:34 -0500

    elimina /api de la URL

--- usuario/usuario-frontend-web/src/services/api.js
+++ usuario/usuario-frontend-web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api', // URL base de tu backend
+    baseURL: 'http://localhost:8080/', // URL base de tu backend
     headers: {
         'Content-Type': 'application/json',
     },
