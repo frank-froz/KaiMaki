f422ee7276d8b0bfe58d2ebf70dfdb69f92cc2f1
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-06-28 18:02:32 -0500

    Agrega las rutas y mejora el aspecto

--- usuario/usuario-frontend-web/src/App.jsx
+++ usuario/usuario-frontend-web/src/App.jsx
@@ -6,48 +6,65 @@ import Dashboard from './pages/Dashboard';
 import LandingPage from './pages/LandingPage';
 import TrabajadoresDisponibles from './pages/TrabajadoresPage';
 import PerfilPage from './pages/PerfilPage';
+import AboutPage from './pages/AboutPage';
+import Footer from './components/Footer';
+import './css/Global.css';
 
 export default function App() {
     return (
         <BrowserRouter>
-            <Routes>
-                {/* Rutas públicas */}
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/trabajadores" element={<TrabajadoresDisponibles />} />
+            <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+                <main style={{ flex: 1 }}>
+                    <Routes>
+                        {/* Rutas públicas */}
+                        <Route path="/" element={<LandingPage />} />
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/register" element={<RegisterPage />} />
+                        <Route path="/about" element={<AboutPage />} />
 
-                {/* Rutas protegidas */}
-                <Route
-                    path="/dashboard"
-                    element={
-                        <PrivateRoute>
-                            <Dashboard />
-                        </PrivateRoute>
-                    }
-                />
+                        {/* Rutas protegidas */}
+                        <Route
+                            path="/dashboard"
+                            element={
+                                <PrivateRoute>
+                                    <Dashboard />
+                                </PrivateRoute>
+                            }
+                        />
+                        <Route
+                            path="/trabajadores"
+                            element={
+                                <PrivateRoute>
+                                    <TrabajadoresDisponibles />
+                                </PrivateRoute>
+                            }  
+                        />
 
-                <Route
-                    path="/perfil"
-                    element={
-                        <PrivateRoute>
-                            <PerfilPage />
-                        </PrivateRoute>
-                    }
-                />
+                        <Route
+                            path="/perfil"
+                            element={
+                                <PrivateRoute>
+                                    <PerfilPage />
+                                </PrivateRoute>
+                            }
+                        />
 
-                <Route
-                    path="/perfil/:id"
-                    element={
-                        <PrivateRoute>
-                            <PerfilPage />
-                        </PrivateRoute>
-                    }
-                />
+                        <Route
+                            path="/perfil/:id"
+                            element={
+                                <PrivateRoute>
+                                    <PerfilPage />
+                                </PrivateRoute>
+                            }
+                        />
 
-                {/* Ruta por defecto si no se encuentra ninguna */}
-                <Route path="*" element={<LandingPage />} />
-            </Routes>
+                        {/* Ruta por defecto si no se encuentra ninguna */}
+                        <Route path="*" element={<LandingPage />} />
+                    </Routes>
+                    </main>
+                <Footer />
+            </div>
         </BrowserRouter>
+        
     );
 }
