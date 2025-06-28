37fec5db2cbbd1b26820b516c0f8dd49f98c8ee4
Han-28 <hector.castro@tecsup.edu.pe>
2025-06-28 18:52:15 -0500

    Ajustes en lógica de trabajadores y autenticación para integración en HomeActivity

--- usuario/usuario-fronted-movil/app/src/main/java/com/kaimaki/usuario/usuario_fronted_movil/data/api/TrabajadorService.kt
+++ usuario/usuario-fronted-movil/app/src/main/java/com/kaimaki/usuario/usuario_fronted_movil/data/api/TrabajadorService.kt
@@ -3,9 +3,11 @@ package com.kaimaki.usuario.usuario_fronted_movil.data.api
 
 import com.kaimaki.usuario.usuario_fronted_movil.domain.model.Trabajador
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 
 interface TrabajadorService {
@@ -20,4 +22,12 @@ interface TrabajadorService {
         @Path("id") id: Long,
         @Header("Authorization") token: String
     ): Call<Trabajador>
+
+    @GET("/api/trabajadores/buscar")
+    suspend fun getTrabajadoresPorOficio(
+        @Header("Authorization") token: String,
+        @Query("oficio") oficio: String
+    ): Response<List<Trabajador>>
+
+
 }
