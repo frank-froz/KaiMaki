e1bc591ee8ea4c58ecd0e5e1549d2ed4bfaddb32
Han-28 <hector.castro@tecsup.edu.pe>
2025-06-28 20:22:03 -0500

    Correcciones en RetrofitInstance y trabajadoresService: ahora el token se envía correctamente

--- usuario/usuario-frontend-web/src/services/trabajadoresService.js
+++ usuario/usuario-frontend-web/src/services/trabajadoresService.js
@@ -4,4 +4,12 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true, // necesario para cookies o tokens que se envían por header o cookie
 });
-export const getTrabajadoresDisponibles = () => api.get('/trabajadores/disponibles');
+export const getTrabajadoresDisponibles = () => {
+  const token = localStorage.getItem("token");
+
+  return api.get('/trabajadores/disponibles', {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+};
