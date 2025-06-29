55ec971483e4da045e58586c2a934ca4ac395020
Kenedy2006 <jeyson.ramos@tecsup.edu.pe>
2025-06-29 00:58:56 -0500

    Se agregó método para buscar otros usuarios para chatear

--- usuario/usuario-backend/src/main/java/com/kaimaki/usuario/usuariobackend/service/UserService.java
+++ usuario/usuario-backend/src/main/java/com/kaimaki/usuario/usuariobackend/service/UserService.java
@@ -2,10 +2,13 @@ package com.kaimaki.usuario.usuariobackend.service;
 
 import com.kaimaki.usuario.usuariobackend.dto.LoginRequestDTO;
 import com.kaimaki.usuario.usuariobackend.dto.UserRegistroDTO;
+import com.kaimaki.usuario.usuariobackend.dto.UserResponseDTO;
 import com.kaimaki.usuario.usuariobackend.model.User;
 
+import java.util.List;
 
 public interface UserService {
     User registrarUsuario(UserRegistroDTO dto) throws Exception;
     User loginUsuario(LoginRequestDTO dto) throws Exception;
+    List<UserResponseDTO> searchUsersByEmail(String email) throws Exception;
 }
