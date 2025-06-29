e9a01d80e18b97f3bf34b85eb2af86f3433c339c
Kenedy2006 <jeyson.ramos@tecsup.edu.pe>
2025-06-29 03:12:02 -0500

    se agrega funciom para que se pueda agregar automáticamente el token JWT de localStorage a todas las peticiones para no pasar el token manualmente en cada función

--- usuario/usuario-frontend-web/src/services/authService.js
+++ usuario/usuario-frontend-web/src/services/authService.js
@@ -4,6 +4,20 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true  // necesario para cookies o tokens que se envían por header o cookie
 })
+
+// Interceptor para agregar automáticamente el token de localStorage
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 export const register = data => api.post('/usuarios/registro', data)
 
 export const login = data => api.post('/usuarios/login', data)
@@ -16,4 +30,3 @@ export const perfil = (token) =>
         Authorization: `Bearer ${token}`,
       },
     });
-
