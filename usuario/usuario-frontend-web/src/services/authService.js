6d72aad5eebb6c5c207c1e4d7fcc856a0578d4ef
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-07-01 03:26:59 -0500

    Mejora la lógica de usar los endpoints v2

--- usuario/usuario-frontend-web/src/services/authService.js
+++ usuario/usuario-frontend-web/src/services/authService.js
@@ -1,32 +1,9 @@
-import axios from 'axios'
+import api from './api';
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true  // necesario para cookies o tokens que se envían por header o cookie
-})
+export const register = (data) => api.post('/usuarios/registro', data);
 
-// Interceptor para agregar automáticamente el token de localStorage
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-export const register = data => api.post('/usuarios/registro', data)
+export const login = (data) => api.post('/usuarios/login', data);
 
-export const login = data => api.post('/usuarios/login', data)
+export const oauthGoogle = (idToken) => api.post('/auth/google', { idToken });
 
-export const oauthGoogle = idToken => api.post('/auth/google', { idToken })
-
-export const perfil = (token) =>
-    api.get('/perfil', {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+export const perfil = () => api.get('/perfil');
